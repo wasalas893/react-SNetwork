35be7348ced5698fa4fe3c1489ccdae26097858e
wasalas <wasalas893@gmail.com>
2020-04-17 16:50:47 +0530

    create profile js for delete user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');     // require express modules
 const router  = express.Router();              // to use express router
 const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -110,5 +110,63 @@ async (req, res) =>{
       }
     }
 );
+
+//@route     POST api/profile
+//@desc      Get all profiles
+//@access    Public
+
+router.get('/',async (req,res)=>{
+   try {
+       const profiles=await Profile.find().populate('user',['name','avatar']);
+       res.json(profiles);
+   } catch (err) {
+       console.error(err.message);
+       res.status(500).send('Server Error');
+   }
+});
+
+//@route     GET api/profile/user/:user_id
+//@desc      Get profile by user ID
+//@access    Public
+
+router.get('/user/:user_id',async (req,res)=>{
+    try {
+        const profile=await Profile.findOne({ user:req.params.user_id}).populate('user',['name','avatar']);
+
+        if(!profile) return res.status(400).json({msg: 'Profile not found'});
+
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind=='ObjectId'){
+            return res.status(400).json({msg: ' Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+ });
+
+ //@route    DELETE api/profile
+//@desc      Delete profile, user & posts
+//@access    Private
+
+router.delete('/',auth, async (req,res)=>{
+    try {
+        //@todo -remove users posts
+
+        //Remove profile
+
+        await Profile.findOneAndRemove({ user:req.user.id });
+        //Remove user
+        await User.findOneAndRemove({ _id:req.user.id });
+
+        res.json({ msg: 'User deleted'});
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+ });
+
+
 
 module.exports = router;
